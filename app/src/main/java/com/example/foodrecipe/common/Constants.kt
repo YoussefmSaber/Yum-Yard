71cafed7acaf561177deca5b4ea7d5d8eabd39b3
Youssef Mohammed <youssef.mu.saber@gmail.com>
2024-12-12 15:59:15 +0200

    Added more endpoints to the project

--- app/src/main/java/com/example/foodrecipe/common/Constants.kt
+++ app/src/main/java/com/example/foodrecipe/common/Constants.kt
@@ -5,4 +5,12 @@ object HttpRoutes {
     const val MEALS = "$BASE_URL/search.php?f=a"
     const val MEAL_ID = "$BASE_URL/lookup.php?i="
     const val MEAL_NAME = "$BASE_URL/search.php?s="
+    const val RANDOM_MEAL = "$BASE_URL/random.php"
+    const val ALL_CATEGORIES_NAMES = "$BASE_URL/categories.php"
+    const val LIST_ALL_CATEGORY = "$BASE_URL/filter.php?c=list"
+    const val LIST_ALL_AREA = "$BASE_URL/filter.php?a=list"
+    const val LIST_ALL_INGREDIENT = "$BASE_URL/filter.php?i=list"
+    const val FILTER_BY_INGREDIENT = "$BASE_URL/filter.php?i="
+    const val FILTER_BY_CATEGORY = "$BASE_URL/filter.php?c="
+    const val FILTER_BY_AREA = "$BASE_URL/filter.php?a="
 }

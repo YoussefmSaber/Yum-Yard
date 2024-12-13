c684098fba4fdfa08fbce87d9a98d21c5580e1c5
Youssef Mohammed <youssef.mu.saber@gmail.com>
2024-12-13 10:34:21 +0200

    Added the icon pack to the project

--- app/src/main/java/com/example/foodrecipe/MainActivity.kt
+++ app/src/main/java/com/example/foodrecipe/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.produceState
 import androidx.compose.ui.Modifier
+import com.example.foodrecipe.common.Iconly
+import com.example.foodrecipe.common.iconly.Google
+import com.example.foodrecipe.common.iconly.`Home-fill`
 import com.example.foodrecipe.domain.model.Category
 import com.example.foodrecipe.domain.usecase.get.general.GetAllCategorizeUseCase
 import com.example.foodrecipe.ui.theme.FoodRecipeTheme
@@ -36,7 +42,9 @@ class MainActivity : ComponentActivity() {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Surface(modifier = Modifier.padding(innerPadding))
                     {
-                        Text(if (meal.value.isNotEmpty()) meal.value[0].category else "No Item found")
+                        Column {
+                            Text(if (meal.value.isNotEmpty()) meal.value[0].category else "No Item found")
+                        }
                     }
                 }
             }
